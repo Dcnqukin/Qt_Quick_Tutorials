f964a4ebffa621d908604744d1d99da2b7fc060b
Dcnqukin <dcn965662867@hotmail.com>
2018-03-04 22:59:55 +0800

    Ip功能测试

---
+++ myIp/widget.cpp
@@ -0,0 +1,31 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+//    QString localHostName = QHostInfo::localHostName();
+//    qDebug() << "localHostName:" << localHostName;
+
+//    QHostInfo info = QHostInfo::fromName(localHostName);
+////    qDebug() << "IP Address:" << info.addresses();
+
+//    foreach(QHostAddress address, info.addresses())
+//    {
+//        if(address.protocol() == QAbstractSocket::IPv4Protocol)
+//            qDebug() << address.toString();
+//    }
+    QHostInfo::lookupHost("www.qter.org", this, SLOT(lookedUp(QHostInfo)));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+void Widget::lookedUp(const QHostInfo &host)
+{
+    qDebug() << host.addresses().first().toString();
+}
